a9116e5fb65dd7a74aead5eeb138ce6d011b7869
Accretionz <jieliu415@gmail.com>
2024-11-02 14:15:59 -0700

    modified:   .gitignore 	new file:   app/admin/page.tsx 	new file:   app/api/sentry-example-api/route.ts 	new file:   app/global-error.tsx 	modified:   app/page.tsx 	new file:   app/patients/[userId]/new-appointment/success/page.tsx 	new file:   components/AppointmentModal.tsx 	new file:   components/PasskeyModal.tsx 	new file:   components/StatCard.tsx 	new file:   components/StatusBadge.tsx 	modified:   components/forms/AppointmentForm.tsx 	new file:   components/table/DataTable.tsx 	new file:   components/table/columns.tsx 	new file:   components/ui/alert-dialog.tsx 	new file:   components/ui/dialog.tsx 	new file:   components/ui/dropdown-menu.tsx 	new file:   components/ui/input-otp.tsx 	new file:   components/ui/table.tsx 	new file:   instrumentation.ts 	modified:   lib/actions/appointment.actions.ts 	modified:   lib/actions/patient.actions.ts 	modified:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   sentry-wizard-installation-error-1730579200958.log 	new file:   sentry-wizard-installation-error-1730579261397.log 	new file:   sentry-wizard-installation-error-1730579341467.log 	new file:   sentry-wizard-installation-error-1730579365847.log 	new file:   sentry-wizard-installation-error-1730579517004.log 	new file:   sentry-wizard-installation-error-1730579547556.log 	new file:   sentry.client.config.ts 	new file:   sentry.edge.config.ts 	new file:   sentry.server.config.ts 	modified:   tailwind.config.ts 	modified:   types/appwrite.types.ts

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -9,7 +9,6 @@ import {InputFile} from "node-appwrite/file"
 export const createUser = async (user: CreateUserParams) => {
     try {
         const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name)
-        console.log({newUser})
     } catch (error: any) {
         if (error && error?.code === 409) {
             const documents = await users.list([
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e3131bd00fd617b18037711359123334@o4508230540918784.ingest.us.sentry.io/4508230543802368",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
